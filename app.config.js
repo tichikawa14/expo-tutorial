e98941a81d61f6a49fb41d3c0756a885cb5fbb00
tichikawa14 <gakusei.umeeee@gmail.com>
2025-09-27 15:19:07 +0900

    EASチュートリアル完

---
+++ app.config.js
@@ -0,0 +1,40 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.tichikawa14.stickersmash.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.tichikawa14.stickersmash.preview';
+  }
+
+  return 'com.tichikawa14.stickersmash';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'StickerSmash (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'StickerSmash (Preview)';
+  }
+
+  return 'StickerSmash: Emoji Stickers';
+};
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
+});
